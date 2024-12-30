412cd2c83c852ab51cbeacc2c0ad1b5d8fee0fed
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-12-30 14:17:37 +0300

    Add project files.

---
+++ CoreMVCIntro_0/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCIntro_0.Controllers
+{
+    public class HomeController : Controller
+    {
+        //MVC'de Controller icerisinde olan metotlara biz Action deriz...
+
+        //MVC'de Action'lar cok önemlidir...Cünük BackEnd işlemlerinizi, HTTP'ye göre vörev yapmak istediginiz tüm islemlerinizi Controller'daki Action'lar yapar...
+        
+        
+        public IActionResult Index()
+        {
+            //View metodu icerisine bir argüman almadıysa , hangi metodun yasam alanında cagrılıyorsa o metodun isminde bir View arar...Yani su anda icinde bulundugu Action'in isminde bir View'i mimarinizin Views klasorunde, icinde bulundugu Controller ile aynı isimde bir klasorun icerisinde arayacaktır...Yani icinde bulundugu ACtion ile aynı isme sahip olan bir View'i döndürmeye calısır. Bu View'i da cagrıldıgı Action'in bulundugu Controller'in ismiyle aynı isimde bir klasorde (Views icerisinde) arar...
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            //Eger View metodunuz string bir argüman alıp baska bir View ismi söylerse dönen View o belirttiginiz isimdeki olur...
+            return View();
+        }
+
+
+        //HomeController hc = new HomeController();
+        // hc.Index();
+    }
+
+    //Tum GUn olsun :3 
+    //Olmasın : 5
+    //Yarım Gün 
+}
---
+++ CoreMVCIntro_0/Program.cs
@@ -0,0 +1,28 @@
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Since when is ambiguity a good design ?
+
+WebApplication app = builder.Build();
+
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles(); //wwwroot'un public olarak kullanılmasına izin verir
+
+app.UseRouting(); //url yapılandırması
+
+app.UseAuthorization(); //Yetkilendirme 
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Contact}/{id?}");
+
+app.Run();
